a2403e40c5bd3fe5c9613eeb5de2d352c3809ff8
clock442 <langstonsconnor@gmail.com>
2020-10-16 12:04:42 -0700

    fixed secret

--- podcastproj/podcastproj/settings.py
+++ podcastproj/podcastproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jk*n%$v0d%l&d@z)&98a%45g$ywt5!s!v(t#5@6)gd2m1(v(vx'
+SECRET_KEY = secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
