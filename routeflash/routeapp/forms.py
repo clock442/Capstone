177a8aadd18bbc620ae2fdf363991d0c10564bbb
clock442 <langstonsconnor@gmail.com>
2020-11-05 12:35:07 -0800

    hidden password

--- routeflash/routeapp/forms.py
+++ routeflash/routeapp/forms.py
@@ -1,12 +1,19 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django import forms
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
 
 class LoginForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
